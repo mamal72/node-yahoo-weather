8605c75e60398656ece1e0a199daa544f34bc6e0
Mohamad Jahani <m4m4lj@gmail.com>
2016-05-31 13:04:44 +0430

    fix: use https instead of http resolves #2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'isomorphic-fetch';
 export default function getWeather(query, unit = 'c') {
-  const queryUri = `http://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where u='${unit}' AND woeid in (select woeid from geo.places(1) where text="${query}")&format=json`;
+  const queryUri = `https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where u='${unit}' AND woeid in (select woeid from geo.places(1) where text="${query}")&format=json`;
   return fetch(queryUri).then(response => {
     if (response.status >= 400) {
       throw new Error('Bad response from server');
